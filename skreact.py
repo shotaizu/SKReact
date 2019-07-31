57b92dd92c08812f0f74f75fe291ffd1fed87144
Alex Goldsack <alex.goldsack1@gmail.com>
2019-07-31 17:58:52 +0900

    Fixed bug where start and end dates were compared as strings not ints, added coloured box showing what region is selected on the lf curves

--- skreact.py
+++ skreact.py
@@ -9,17 +9,24 @@ __author__ = "Alex Goldsack"
     oscillation, interaction and detection.
 """
 
+from params import *
 from reactor import Reactor
 from tkinter import *
-from params import *
+from tkinter import messagebox # Wildcard doesn't import for some reason
+import tkinter.ttk as ttk
+
 # Lots of matplotlib gubbins to embed into tkinter
-import matplotlib as plt
-plt.use("TkAgg") 
+import matplotlib
+matplotlib.use("TkAgg") 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.dates as mdates
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
-import tkinter.ttk as ttk
+
 import pandas as pd
 import numpy as np
+import datetime as dt
 import os
 
 # Getting all reactor power information into pd df
@@ -100,28 +107,61 @@ def main():
     n_nu_lbl = ttk.Label(skreact_win, text = "n_nu")
     n_nu_lbl.grid(column=1, row=2)
 
+    # Setting up plot of monthly load factors
+    lf_labelframe = ttk.Labelframe(skreact_win, text = "Reactor Monthly Load Factors")
+    lf_labelframe.grid(column=1, row=3)
+    lf_fig = Figure(figsize=(5,4), dpi=100)
+    lf_ax = lf_fig.add_subplot(111)
+    # Load factor is a %age which occasionally goes over 100
+    lf_ax.set_ylim(0,110)
+    lf_canvas = FigureCanvasTkAgg(lf_fig, master=lf_labelframe)
+    lf_canvas.get_tk_widget().grid(column=1, row=3)
+
     # Updating label with n_nu for selected reactor/period
     def update_n_nu(event):
         global start_year
-        start_year = start_year_combo.get()
+        start_year = int(start_year_combo.get())
         global start_month
-        start_month = start_month_combo.get()
+        start_month = int(start_month_combo.get())
         global end_year
-        end_year = end_year_combo.get()
+        end_year = int(end_year_combo.get())
         global end_month
-        end_month = end_month_combo.get()
+        end_month = int(end_month_combo.get())
         # print(str(start_year) + "/" + str(start_month))
         # print(str(end_year) + "/" + str(end_month))
         if(end_year < start_year
                 or (end_year == start_year and end_month < start_month)):
                     n_nu_lbl["text"] = "Start period after end period"
+                    print(end_year == start_year)
+                    print(end_month < start_month)
+                    # print(start_year)
+                    print(start_month)
+                    # print(end_year)
+                    print(end_month)
         else:
-            period = str(start_year)+"/"+str(start_month).zfill(2)+"-"+str(end_year)+"/"+str(end_month).zfill(2)
+            period = "%i/%02i-%i/%02i" % (start_year, start_month, end_year, end_month)
             selected_reactor_name = reactors_combo.get()
             selected_reactor =  next((
                 reactor for reactor in reactors if reactor.name == selected_reactor_name), None)
             n_nu = selected_reactor.n_nu(period = period)
             n_nu_lbl['text'] = ("n_nu = %.2E" % n_nu)
+            lf_ax.clear()
+            # For some reason when plotting dates it uses months as ints
+            start_int = (int(start_year)-2015)*12+int(start_month)-1
+            end_int = (int(end_year)-2015)*12+int(end_month)-1
+            width_int = end_int - start_int
+            # Box showing period selected
+            # width to show inclusivity, starting from start of "bin"
+            period_box = patches.Rectangle(
+                    (start_int-0.5,0), width=width_int+1, height=110, alpha=0.2)
+            lf_ax.add_patch(period_box)
+
+            try:
+                selected_reactor.lf_monthly.plot(ax=lf_ax, marker=".")
+            except TypeError:
+                messagebox.showinfo("LF Plot Error", "No numeric load factor data to plot! (Check .xls file)")
+            lf_fig.autofmt_xdate()
+            lf_canvas.draw()
 
     # Binding changing any info to update the number of nu 
     start_year_combo.bind("<<ComboboxSelected>>", update_n_nu)
@@ -130,18 +170,6 @@ def main():
     end_month_combo.bind("<<ComboboxSelected>>", update_n_nu)
     reactors_combo.bind("<<ComboboxSelected>>", update_n_nu)
 
-    # Plotting the monthly load factors for the selected reactor
-    lf_labelframe = ttk.Labelframe(skreact_win, text = "Reactor Monthly Load Factors")
-    lf_labelframe.grid(column=1, row=3)
-    lf_fig = Figure(figsize=(5,4), dpi=100)
-    lf_ax = lf_fig.add_subplot(111)
-
-    lf_monthlys = [reactor.lf_monthly for reactor in reactors]
-    lf_monthlys[0].plot(ax=lf_ax)
-
-    lf_canvas = FigureCanvasTkAgg(lf_fig, master=lf_labelframe)
-    lf_canvas.draw()
-    lf_canvas.get_tk_widget().grid(column=1, row=3)
 
     # Run the window
     skreact_win.mainloop()
