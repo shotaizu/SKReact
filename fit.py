eca7f83c53ad4b0c813b02adddc14266aea363a0
Alex Goldsack <alex.goldsack1@gmail.com>
2019-11-13 02:03:17 +0900

    Fixed chi square calc.

--- fit.py
+++ fit.py
@@ -4,6 +4,7 @@ from smear import Smear
 from tkinter import *
 from tkinter import filedialog
 import tkinter.ttk as ttk
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
@@ -20,19 +21,52 @@ def fit_win(import_filename,reactors,period,wit_smear):
     import_dat_df.columns = ["energy","bin_content"]
     import_dat = import_dat_df.set_index("energy")["bin_content"]
 
+    # Area normalise
+    import_dat_int = np.trapz(import_dat)
+    import_dat_norm = import_dat.apply(
+        lambda x: x/import_dat_int)
+
+    import_dat_norm_int = np.trapz(import_dat_norm)
+
     # Now make window
     # fit_win = Toplevel(skreact_win)
     fit_win = Tk()
     fit_win.title("Import and fit data")
 
+    # Return chi^2 for given smeared spec
+    def chi_square(smear_spec):
+        # Empty list of energies matching imported spec
+        energies_series = pd.Series(
+            np.nan, index=import_dat.index
+        )
+        # Don't double count the smeared spec
+        energies_series = energies_series[
+            ~energies_series.index.isin(smear_spec.index)]
+        # Concat with smeared spec
+        inter_smear_dat = pd.concat([smear_spec, energies_series])
+        inter_smear_dat.sort_index(inplace=True)
+        print(inter_smear_dat)
+        # Interpolate to fill the new values between and AFTER import points
+        inter_smear_dat.interpolate(method="linear", 
+            limit_direction="both", 
+            inplace=True)
+        print(inter_smear_dat)
+
+        # Get rid of extra points we don't want from original smear spec
+        inter_smear_dat = inter_smear_dat[
+            inter_smear_dat.index.isin(import_dat.index)]
+        print(inter_smear_dat)
+
+        # Normalise smeared plot
+        inter_smear_dat_int = np.trapz(inter_smear_dat)
+        inter_smear_dat_norm = inter_smear_dat.apply(
+            lambda x: x/inter_smear_dat_int)
+        diff_dat = import_dat_norm.subtract(inter_smear_dat_norm)
+        diff_sq_dat = diff_dat.apply(lambda x: x**2)
+
+        return diff_sq_dat.sum()
+
     def fit_data(*args):
-        # Area normalise
-        import_dat_int = np.trapz(import_dat)
-        import_dat_norm = import_dat.apply(
-            lambda x: x/import_dat_int)
-
-        import_dat_norm_int = np.trapz(import_dat_norm)
-
         def fit_recursive(fit_check_var_index=0):
             # If there are no more parameters to fit
             if(fit_check_var_index >= len(fit_check_vars)):
@@ -49,7 +83,6 @@ def fit_win(import_filename,reactors,period,wit_smear):
             return
 
         total_int_spec = pd.Series(0, index=ENERGIES)
-        print(pd.Series(0, index=ENERGIES))
 
         # Start at minimum values of defined range, calc chi-square
         for reactor in reactors:
@@ -64,36 +97,12 @@ def fit_win(import_filename,reactors,period,wit_smear):
             total_int_spec = total_int_spec.add(int_spec)
         smear_spec = wit_smear.smear(total_int_spec, "nu")
 
-        # Empty list of energies matching imported spec
-        energies_series = pd.Series(
-            np.nan, index=import_dat.index
-        )
-        energies_series = energies_series[
-            ~energies_series.index.isin(import_dat.index)]
-        # Concat with smeared spec
-        inter_smear_dat = pd.concat([smear_spec, energies_series])
-        inter_smear_dat.sort_index(inplace=True)
-        # Interpolate to fill the new values between and AFTER import points
-        inter_smear_dat.interpolate(method="linear", 
-            limit_direction="both", 
-            inplace=True)
-
-        # Get rid of extra points we don't want from original smear spec
-        inter_smear_dat = inter_smear_dat[
-            inter_smear_dat.index.isin(import_dat.index)]
-
-        # Normalise smeared plot
-        inter_smear_dat_int = np.trapz(inter_smear_dat)
-        inter_smear_dat_norm = inter_smear_dat.apply(
-            lambda x: x/inter_smear_dat_int)
-        diff_dat = import_dat_norm.subtract(inter_smear_dat_norm)
-        diff_sq_dat = diff_dat.apply(lambda x: x**2)
-
         # List of parameter values and their chi-squared to the data
         fit_dat = [[DM_21_MIN,THET_12_MIN,DM_31_MIN,THET_13_MIN,
-            diff_sq_dat.sum()]]
-        
-        exit()
+            chi_square(smear_spec)]]
+
+        # Stores the param centre and range for a given cycle
+        param_info = []
         
         for i in range(N_CYCLES):
             print("Fit cycle %i" % i)
